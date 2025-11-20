67ac69748b2788809d82f968f8d449caf3bee0da
kavindu <kavindusadunil@gmail.com>
2025-11-21 01:32:15 +0530

    i add reverse proxy chnages

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { Product } from "../types/Product";
 
-const API_URL = "/api/products";
+const API_URL = "http://127.0.0.1:5000/api/products";
 
 export const getProducts = async (): Promise<Product[]> => {
   const res = await axios.get<Product[]>(API_URL);
