2c5e55665704cae276246fa14dcbcc5566a83c30
kavindu <kavindusadunil@gmail.com>
2025-11-29 00:07:51 +0530

    chnage route

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { Product } from "../types/Product";
 
-const API_URL = "oyu/api/products";
+const API_URL = "/api/products";
 
 export const getProducts = async (): Promise<Product[]> => {
   const res = await axios.get<Product[]>(API_URL);
