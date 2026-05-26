d0652b90485ed0f3763ff83790d1c92c9236cc85
kavindu <kavindusadunil@gmail.com>
2026-05-26 09:04:45 +0530

    trigger deploy

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { Product } from "../types/Product";
 
-const API_URL = "http://localhost:5000/products";//"http://localhost:5000/products";  //when server need "/api/products";
+const API_URL = "/api/products";//"http://localhost:5000/products";  //when server need "/api/products";
 
 export const getProducts = async (): Promise<Product[]> => {
   const res = await axios.get<Product[]>(API_URL);
