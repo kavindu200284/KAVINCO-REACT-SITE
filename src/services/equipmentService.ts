98830dbed9cbd481f01c7d73a7ebf65f9dc72972
kavindu <kavindusadunil@gmail.com>
2026-07-05 01:08:22 +0530

    changes doing for equipments checking

--- src/services/equipmentService.ts
+++ src/services/equipmentService.ts
@@ -1,10 +1,8 @@
 import axios from "axios";
 import { Equipment } from "../types/Equipment";
 
-const BASE_URL =
-  import.meta.env.VITE_API_BASE_URL ||
-  (window.location.hostname === "localhost" ? "http://localhost:5000" : "https://kavinco-react-backend.onrender.com");
-const API_URL = `${BASE_URL}/api/equipment`;
+
+const API_URL = "https://kavinco-react-backend.onrender.com/api/equipment"
 
 export const getEquipment = async (): Promise<Equipment[]> => {
   const res = await axios.get<Equipment[]>(API_URL);
